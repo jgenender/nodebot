7af518597fdf0fbde1ff890d7b79dac62e13526a
Jeff Genender <jgenender@savoirtech.com>
2022-10-01 22:27:50 -0600

    Add proper logging with rolling, update gitignore for logs

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,21 @@
 import {App, LogLevel} from '@slack/bolt';
+import log4js from "log4js";
+import log4jconfig from './config/log4js.json'
 
 require("dotenv").config();
 
-const glob = require('Glob');
+//Inititalize logger
+export const logger = log4js.configure(log4jconfig).getLogger();
 
+//Set up plugins
 interface PluginType {
     [name: string]: any;
 }
 
 const plugins = new Array<PluginType>();
 
+//Load plugins
+const glob = require('Glob');
 glob.sync(`./plugins/*(*.ts|*.js)`, {cwd: __dirname}).map((file: string) => {
     let plugin = require(file);
     plugins[plugin.name] = plugin;
@@ -21,9 +27,10 @@ const app = new App({
     signingSecret: process.env.SLACK_SIGNING_SECRET,
     socketMode: true, // enable the following to use socket mode
     appToken: process.env.APP_TOKEN,
-    logLevel: LogLevel.DEBUG,
+    logLevel: LogLevel.INFO,
 });
 
+// Create message handling dispatcher
 app.message('~', async ({message, say}) => {
     if (message.subtype === undefined || message.subtype === 'bot_message') {
         if (message.text && message.text.startsWith("~")) {
@@ -31,7 +38,6 @@ app.message('~', async ({message, say}) => {
             const item = message.text.match(re);
             if (item) {
                 const key: string = item[0].substring(1);
-                console.log("=========>" + key);
                 if (plugins[key as any]) {
                     plugins[key as any].func(message, say);
                 } else {
@@ -45,9 +51,9 @@ app.message('~', async ({message, say}) => {
     }
 });
 
-console.log('Starting Nodebot...');
+logger.info('Starting Nodebot...');
 (async () => {
     // Start your app
     await app.start();
-    console.log(`Nodebot is running!`);
+    logger.info(`Nodebot is running!`);
 })();
--- src/plugins/hello.ts
+++ src/plugins/hello.ts
@@ -1,8 +1,10 @@
 import {BotMessageEvent, GenericMessageEvent, SayFn} from "@slack/bolt";
+import {logger} from "../app";
 
 exports.name = 'hello';
 exports.func = async function(message: GenericMessageEvent | BotMessageEvent, say: SayFn) {
-    if (message.text && message.text.startsWith("~hello")) {
+    logger.debug(message);
+    if (message.text && message.text.startsWith(`~${this.name}`)) {
         say(`Hello, <@${message.user}>`);
     }
 }
